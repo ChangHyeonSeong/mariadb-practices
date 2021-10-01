c1ed5f1cb4fd71e3c5bc9c3ef072b401d89c4258
eogkdfh <eogkdfh@gmail.com>
2021-10-01 12:13:49 +0900

    order by

--- sql-practices/sql연습/select기본.sql
+++ sql-practices/sql연습/select기본.sql
@@ -74,6 +74,26 @@ from employees
 where hire_date between '1989-01-01'and '1989-12-31'
 order by hire_date;
 
+-- order by 절 #1
+-- 예제1: 남자 직원의 전체 이름, 성별, 입사일을 입사일순(선임순)으로 출력
+select first_name,gender,hire_date
+from employees
+where gender = 'm'
+order by hire_date desc;
+
+-- 예제2: 직원들의 사번, 월급을 사번, 월급 순으로 출력
+select emp_no, salary, from_date, to_date
+from salaries
+order by emp_no asc, salary desc;
+
+
+
+
+
+
+
+
+
 
 
 
