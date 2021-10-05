86ce87a01c9e4bd94918798bb8d053ada5e8c03c
eogkdfh <eogkdfh@gmail.com>
2021-10-06 01:20:30 +0900

    join

--- sql-practices/sql연습/조인연습.sql
+++ sql-practices/sql연습/조인연습.sql
@@ -100,6 +100,20 @@ where de.emp_no = s.emp_no
 group by d.dept_name;
 
 
+-- 예제7: 현재 직책별로 급여의 총합을 구하되 Engineer직책은 제외하세요
+--        단, 총합이 2,000,000,000이상인 직책만 나타내며 급여총합에
+--        대해서 내림차순(DESC)로 정렬하세요.   
+select t.title, sum(salary) as sum_salary
+from titles t, salaries s
+where t.emp_no = s.emp_no
+  and t.to_date = '9999-01-01'
+  and s.to_date = '9999-01-01'
+  and t.title != 'Engineer'
+group by t.title
+having  sum_salary >= 2000000000
+order by sum_salary desc;
+
+
 
 
 
